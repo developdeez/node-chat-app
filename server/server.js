a785572c655c02fa11e49e0b68703bce3798e7e8
Cecil Carter <ccarter@HME.COM>
2018-04-11 15:44:17 -0700

    added test for socket message and function

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage}=require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,39 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage',
-  {
-    from:'admin@mike.com',
-    text:'Welcome to the chat',
-    createdAt:123
-  });
+  socket.emit('newMessage',generateMessage('Admin','Welcome creeper'));
 
-  socket.broadcast.emit('newMessage',
-{
-  from:'admin@mike.com',
-  text:'Hey whats up Someone is creeping',
-  createdAt:123
-});
-
-// socket.emit('newMessage',
-// {
-//   from:'mike@mike.com',
-//   text:'Hey whats up',
-//   createdAt:123
-// });
+  socket.broadcast.emit('newMessage',generateMessage('Admin','Someone creeping'));
 
 socket.on('createMessage',(newMessage)=>{
 console.log('creating message',newMessage);
-io.emit('newMessage',{
-from:newMessage.from,
-text:newMessage.text,
-createdAt:new Date().getTime()
-});
-// socket.broadcast.emit('newMessage',{
-//   from:newMessage.from,
-//   text:newMessage.text,
-//   createdAt:new Date().getTime()
-// });
+io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
 });
 
   socket.on('disconnect', () => {
